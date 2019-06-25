bad3897a7d217ba6936f368d2894f2e09bfb16f0
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-25 14:38:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,8 @@ def happy_birthday(birthday_kids)
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    binding.pry
     if age <= 12
+      binding.pry
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
